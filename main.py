78cc028e407592b4551ac270b7439e970e0a6b88
Amir Dehestani <amira.dehestani@gmail.com>
2025-09-22 17:28:15 -0400

    Fix text extraction to replace newlines with spaces in PDF processing in order to remove unnecessary pauses

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def extract_text_from_pdf(pdf_path):
     reader = PdfReader(pdf_path)
     text = ""
     for page in reader.pages:
-        text += page.extract_text() + "\n"
+        text += page.extract_text().replace("\n", " ") + "\n"
     return text
 
 
